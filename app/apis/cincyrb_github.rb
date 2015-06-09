8e35bbf5d5ad01ef9c96eea6580440a3d6b4cbde
Vincent Wilson <vincent.movieman@gmail.com>
2015-06-08 21:40:15 -0400

    Quick and dirt build of github api

---
+++ app/apis/cincyrb_github.rb
@@ -0,0 +1,20 @@
+class CincyrbGithub
+  include HTTParty
+
+  base_uri 'https://api.github.com/'
+
+  def self.get_contributors
+    get("/repos/cincinnatirb/cincyrb/contributors", headers: {"User-Agent" =>"Cincyrb"})
+  end
+
+  def self.get_top_contributor
+    self.get_contributors.first
+  end
+
+  def self.get_all_avatars
+    # self.get_contributors.map(&:avatar_url)
+    self.get_contributors.map { |user| user["avatar_url"] } 
+  end
+
+end
+
